cd4fd3878f43ff005c20a2397ef56eb598691005
Ruslana Onyshchuk <lalelya7@gmail.com>
2023-09-19 14:44:29 +0200

    Update contacts-controllers.js

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,9 +1,15 @@
-import { ctrlWrapper } from "../decorators/index.js";
+import { ctrlWrapper } from "../decorators/index.js"
 import { HttpError } from "../helpers/index.js";
-import contacts from "../models/contactsDB.js";
+import contacts from "../models/contactsDB.js"
 
 const getAllContacts = async (req, res, next) => {
-  const contactList = await contacts.listContacts()
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 1, favorite } = req.query;
+  const filterObject = favorite ? { owner, favorite } : { owner };
+  const skip = (page - 1) * limit;
+  const params = { skip, limit, }
+  
+  const contactList = await contacts.listContacts(filterObject, params)
   if (!contactList) throw HttpError(404, 'Not found')
 
   res.json({
@@ -14,56 +20,57 @@ const getAllContacts = async (req, res, next) => {
 }
 
 const getContactsById = async (req, res, next) => {
-    const contactId = req.params.contactId
-    const currentContact = await contacts.getContactById(contactId)
+  const contactId = req.params.contactId
+  const currentContact = await contacts.getContactById(contactId)
 
-    if (!currentContact) throw HttpError(404, 'Not found')
+  if (!currentContact) throw HttpError(404, 'Not found')
 
-    res.json({
-      status: "OK",
-      code: 200,
-      data: currentContact,
-    })
+  res.json({
+    status: "OK",
+    code: 200,
+    data: currentContact,
+  })
 }
 
 const deleteContactsById = async (req, res, next) => {
-    const contactId = req.params.contactId
-    const currentContact = await contacts.removeContact(contactId)
+  
+  const contactId = req.params.contactId
+  const currentContact = await contacts.removeContact(contactId)
 
-    if (!currentContact) throw HttpError(404, 'Not found')
+  if (!currentContact) throw HttpError(404, 'Not found')
 
-    res.json({
-      status: "OK",
-      code: 200,
-      data: currentContact,
-    })
+  res.json({
+    status: "OK",
+    code: 200,
+    data: currentContact,
+  })
 }
 
 const createContact = async (req, res, next) => {
-    const { name, email, phone } = req.body
+  const { name, email, phone } = req.body
 
-    const currentContact = await contacts.addContact({ name, email, phone })
-    if (!currentContact) throw HttpError(404, 'Not found')
+  const currentContact = await contacts.addContact({ name, email, phone })
+  if (!currentContact) throw HttpError(404, 'Not found')
 
-    res.json({
-      status: "Created",
-      code: 201,
-      data: currentContact,
-    })
+  res.json({
+    status: "Created",
+    code: 201,
+    data: currentContact,
+  })
 }
 
 const updateContact = async (req, res, next) => {
-    const { contactId } = req.params
-    const { name, email, phone } = req.body
+  const { contactId } = req.params
+  const { name, email, phone } = req.body
 
-    const currentContact = await contacts.updateContact(contactId, { name, email, phone })
-    if (!currentContact) throw HttpError(404, 'Not found')
+  const currentContact = await contacts.updateContact(contactId, { name, email, phone })
+  if (!currentContact) throw HttpError(404, 'Not found')
 
-    res.json({
-      status: "OK",
-      code: 200,
-      data: currentContact,
-    })
+  res.json({
+    status: "OK",
+    code: 200,
+    data: currentContact,
+  })
 }
 
 const updateStatusContact = async (req, res, next) => {
@@ -85,9 +92,8 @@ const updateStatusContact = async (req, res, next) => {
 export default {
   getAllContacts: ctrlWrapper(getAllContacts),
   getContactsById: ctrlWrapper(getContactsById),
-  deleteContactsById: ctrlWrapper(deleteContactsById), 
-  createContact: ctrlWrapper(createContact), 
-  updateContact: ctrlWrapper(updateContact), 
+  deleteContactsById: ctrlWrapper(deleteContactsById),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
   updateStatusContact: ctrlWrapper(updateStatusContact),
-
 }
